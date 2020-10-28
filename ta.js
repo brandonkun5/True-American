aec7653bc0771701ad789d4b217d64d4238549f2
brandonkun5 <64926701+brandonkun5@users.noreply.github.com>
2020-10-28 18:56:26 -0400

    Add files via upload

---
+++ ta.js
@@ -0,0 +1,7 @@
+function card(name, type, description, amtPulled, skips) {
+    this.name = name,
+    this.type = type,
+    this.description = description,
+    this.amtPulled = amtPulled,
+    this.skips = skips
+}
