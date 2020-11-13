9d091cde324f1a5c630951faa43b8e5bafe7dade
brandonkun5 <64926701+brandonkun5@users.noreply.github.com>
2020-11-12 19:53:45 -0500

    Update ta.js

--- ta.js
+++ ta.js
@@ -1,3 +1,5 @@
+
+// Object-scheme for each card
 function card(name, type, description, amtPulled, skips, deckCount) {
     this.name = name,
     this.type = type,
@@ -7,6 +9,7 @@ function card(name, type, description, amtPulled, skips, deckCount) {
     this.deckCount = deckCount
 }
 
+//List of cards
 var prohibitionCard= new card('Prohibition', 'miniGame', 'Drink a shot of moonshine', 0, 0, 2);
 var woodstockCard= new card('Woodstock', 'miniGame', 'Choose a song, first team to guess it wins. All other teams drink.', 0, 0, 2);
 var bostontPartyCard= new card('Boston Tea Party', 'miniGame', 'Put your pinkies in the air, last one has to drink', 0, 0, 2);
@@ -23,9 +26,9 @@ var flipCard= new card('Flip Cup', 'bigGame', 'Play flip cup, thats it', 0, 0, 2
 var vegasCard= new card('Vegas Baby', 'bigGame', 'Players start their turn by wagering an amount of their drink by pouring it into the glass in the center of the table. Once they are satisfied with the amount they wagered, they will call red or black. The top card of the deck is flipped. If they were right the turn passes to the next player, but if they are wrong, they have to drink whatever is in the glass in the center, and not just a sip, they have to drink all of it.', 0, 0, 2);
 var rbgCard= new card('Ruth Bader Ginsberg', 'bigGame', 'Grab 20-30 solo cups, fill them up at least 1/3rd with beer, and place them in the center of a round table. Pick up one more cup to be the center cup and fill it completely with a full beer. Disperse all players evenly around the table, pick two players to start the game, and give them each a pong ball. The game begins with a backward “3, 2, 1-” countdown. After “1”, the two players with the pong balls pick a cup closest to them, chug the beer, and attempt to rapidly bounce their ball into the empty cup. Once they succeed, they will pass the cup and the ball clockwise to the next player, and this continues until a player makes it into their cup while the next person is still bouncing. Once this happens, the player will stack their cup onto the next one, and the losing player will pass their stack clockwise, pick a new cup, drink the beer, and proceed playing. The game ends when all of the cups have been consumed (ending with the center cup that contains a full beer).', 0, 0, 2);
 var civilWarCard= new card('Civil War', 'bigGame', 'Two teams of 3 face each other from across the table. Each player has their own 3 or 6 cup lifeline, arranged in the 3-2-1 triangle format. 2 water cups per team is recommended, as this game is much more fun (and faster paced) if beer is used inside the cups, although it is optional that a player use water as long as they drink for each cup made. Two teams of three square off in a rapid-fire game of pong, where each player has a 6 cup lifeline (3 can work if the table is small), and no balls are off limit for roll backs (so pay attention to rebounds). The drink-before-you-shoot rule is in play as usual, but there is no "turns", so any team/player can shoot any ball at any time, including passing balls off to their teammates (if for instance their cup is made and they have to drink). The first team to make all of the opposing teams cups wins. As its extremely unlikely that all three players lose at the same time, when players are knocked out of a game of civil war, they can rebound balls for their team (usually the 50/50 rule is enforced, where they cant step on their opponents side of the room to rebound a ball (but reaching is allowed while feet remain on their side). This adds an additional element of strategy to the game, as it might make more sense to leave a player with 1 cup in the game so he wont focus solely on rebounding after being shot out.', 0, 0, 2);
-
+var gameOverCard= new card('Game Over', 'finish', 'You went through the whole deck! To keep going, just hit the start game button', 0, 0, 0)
 var listOfCards = [prohibitionCard, woodstockCard, bostontPartyCard, barackObamaCard, southCard, monicaLCard,
-souCard, sbaCard, illuminatiCard, abeCard, coldWarCard, capitalCard, flipCard, vegasCard, rbgCard, civilWarCard];
+    souCard, sbaCard, illuminatiCard, abeCard, coldWarCard, capitalCard, flipCard, vegasCard, rbgCard, civilWarCard];
 
 
 function deck() {
@@ -41,29 +44,46 @@ function fillDeck(cards, emptydeck) {
         }
 
     } )
-    console.log(emptydeck);
+    shuffleCards();
 };
-
-// Pulled this shuffle function from
-// https://medium.com/@blakeeh723/how-to-build-a-card-game-with-object-oriented-programming-c43cd2cadb3a
-function shuffleDeck(full_deck) {
-    let location1, location2, tmp;
-    for (let i = 0; i < 1000; i++) {
-        location1 = Math.floor(( Math.random() * full_deck.cards.length ));
-        location2 = Math.floor(( Math.random() * full_deck.cards.length ))
-
-        tmp = full_deck.cards[location1];
-        full_deck.cards[location1] = full_deck.cards[location2];
-        full_deck.cards[location2] = tmp;
-    }
-}
 
 var gamedeck = new deck();
 
-fillDeck(listOfCards, gamedeck);
-shuffleDeck(gamedeck);
+document.getElementById("startGame").addEventListener("click", fillDeck(listOfCards, gamedeck));
 
-gamedeck.cards.forEach((c) => {
-    console.log(c.name);
-})
-console.log(gamedeck.cards);
+function shuffleCards() {
+    function shuffle(array) {
+        var j, x, i; 
+    
+        for (i = array.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = array[i];
+            array[i] = array[j];
+            array[j] = x;
+        }
+    
+        return array;
+    };
+
+    shuffle(gamedeck.cards);
+}
+    
+document.getElementById("nextCard").addEventListener("click", next);
+
+function next() {
+    if (gamedeck.cards.length == 0) {
+        var card = gameOverCard
+    }
+    else {
+        var card = gamedeck.cards.pop()
+        cardName = card.name;
+        console.log(gamedeck)
+    }
+    $('h5#cardTitle').text(card.name);
+    $('p#cardText').text(card.description);
+
+}
+
+
+
+
